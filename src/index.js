2520b26dd70b158e30e583c78eee228c992bc2c4
Timo Nam <dwbh7358@gmail.com>
2023-01-26 23:20:39 +0900

    feat: implement deleteToDo in To Do App ✨

--- src/index.js
+++ src/index.js
@@ -7,18 +7,42 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
 };
 
 const store = legacy_createStore(reducer);
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
 
 const paintToDos = () => {
   const toDos = store.getState();
@@ -27,18 +51,20 @@ const paintToDos = () => {
 
   toDos.forEach((toDo) => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+
     li.id = toDo.id;
     li.innerText = toDo.text;
 
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+
+    li.appendChild(btn);
     ul.appendChild(li);
   });
 };
 
 store.subscribe(paintToDos);
-
-const addToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
-};
 
 const onSubmit = (e) => {
   e.preventDefault();
@@ -46,7 +72,7 @@ const onSubmit = (e) => {
   const toDo = input.value;
   input.value = "";
 
-  addToDo(toDo);
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
