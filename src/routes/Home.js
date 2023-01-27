a81e5af70b92ce33692873044caf263f89a182ca
남이찬 <dwbh7358@sbs.co.kr>
2023-01-27 10:08:19 +0900

    feat: add store, apply mapStateToProps to Home ✨

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
 
   function onChange(e) {
@@ -19,8 +20,13 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { legacy_createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+const addToDo = (text) => {
+  return {
+    type: ADD,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    id,
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ type: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = legacy_createStore(reducer);
+
+export default store;
